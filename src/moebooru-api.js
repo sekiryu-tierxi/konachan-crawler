9fb0b63fa4de813b0e65d522059f38e8decf28b8
Sekiryu Tier XI <121081570+sekiryu-tierxi@users.noreply.github.com>
2023-01-05 20:19:45 +0800

    style(moebooru): use class instead of prototype

--- src/moebooru-api.js
+++ src/moebooru-api.js
@@ -2,41 +2,31 @@ import got, { Options } from 'got'
 
 function filterSingleImageUrl(image) {
 	const MEGABYTE_CONSTANT = 1024 * 1024
-	if (image.fileSize / MEGABYTE_CONSTANT < 10) return image.fileUrl
-	if (image.jpegSize / MEGABYTE_CONSTANT < 10) return image.jpegUrl
-	return image.sampleUrl
+	if (image.file_size / MEGABYTE_CONSTANT < 10) return image.file_url
+	if (image.jpeg_file_size / MEGABYTE_CONSTANT < 10) return image.jpeg_url
+	return image.sample_url
 }
 
-export const MoebooruInstance = function (baseUrl) {
-	this.gotOptions = new Options({
-		prefixUrl: baseUrl,
-	})
-	this.gotOptions.freeze()
-}
-
-MoebooruInstance.prototype.getImageList = function (limit) {
-	return got('post.json', { searchParams: { limit } }, this.gotOptions).json()
-}
-
-MoebooruInstance.prototype.compressImageList = function (imageList) {
-	return imageList.map((image) => ({
-		score: image.score,
-		jpegSize: image.jpeg_file_size,
-		jpegUrl: image.jpeg_url,
-		sampleSize: image.sample_file_size,
-		sampleUrl: image.sample_url,
-		fileSize: image.file_size,
-		fileUrl: image.file_url,
-	}))
-}
-
-MoebooruInstance.prototype.filterImageUrl = function (imageList) {
-	return imageList.map((image) => ({
-		score: image.score,
-		url: filterSingleImageUrl(image),
-	}))
-}
-
-MoebooruInstance.prototype.sortImageList = function (imageList) {
-	return imageList.sort((a, b) => b.score - a.score)
+export class MoebooruInstance {
+	constructor(baseUrl) {
+		this.gotOptions = new Options({
+			prefixUrl: baseUrl,
+		})
+		this.gotOptions.freeze()
+	}
+	getImageList(limit = 20) {
+		return got('post.json', { searchParams: { limit } }, this.gotOptions).json()
+	}
+	getPopularImageList() {
+		return got('post/popular_recent.json', undefined, this.gotOptions).json()
+	}
+	static filterImageUrl(imageList) {
+		return imageList.map((image) => ({
+			score: image.score,
+			url: filterSingleImageUrl(image),
+		}))
+	}
+	static sortImageList(imageList) {
+		return imageList.sort((a, b) => b.score - a.score)
+	}
 }
