a7e04c095d2bc59948e14880be64fa99581d5b9b
Sekiryu Tier XI <121081570+sekiryu-tierxi@users.noreply.github.com>
2023-01-06 15:08:47 +0800

    fix(main): remove extra newline in error logging

--- src/main.js
+++ src/main.js
@@ -39,7 +39,7 @@ while(retry <= options.retryLimit) {
     await bot.run()
     process.exit(0)
   } catch (e) {
-    console.error(e.code + '\n')
+    console.error(e.code)
     console.error(e.response.body)
     retry++
     const current = Date.now()
